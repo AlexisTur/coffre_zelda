80ff65bfcd4765fbc15a18759f350331412873bd
Alexis <aleis.turmeau.2@gmail.com>
2026-05-07 11:27:16 +0200

    test sur test pour module mp3 - result : fonctionne

--- arduino/prog_v1/prog_v1.ino
+++ arduino/prog_v1/prog_v1.ino
@@ -1,26 +1,28 @@
+
+
+//  ESP32dfmini01
+//
 #include "HardwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 #include "Arduino.h"
 
-// GPIO sûrs sur ESP32-S3 SuperMini
-const byte RXD2 = 7;   // ESP32 RX ← TX du DFPlayer
-const byte TXD2 = 6;   // ESP32 TX → RX du DFPlayer
+const byte RXD2 = 7;   // ESP32 RX  -> TX du module
+const byte TXD2 = 6;   // ESP32 TX  -> RX du module
 
-HardwareSerial dfSD(1);  // UART1 (pas 3 !)
+HardwareSerial dfSD(1); // UART1
 DFRobotDFPlayerMini player;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);          // moniteur série plus rapide
   delay(2000);
 
   dfSD.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  delay(3000);
+  delay(3000);                   // laisser le temps au DFPlayer de booter
 
   Serial.println("Init DFPlayer...");
-  //delay(5000);
   if (player.begin(dfSD)) {
     Serial.println("DFPlayer OK");
-    player.volume(17);
+    player.volume(17);           // 0..30
   } else {
     Serial.println("Connecting to DFPlayer Mini failed!");
   }
